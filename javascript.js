43b8931b587fca1ebdb886388915d5d8c9ea138c
TristanNguyen0 <xkaura64@gmail.com>
2023-06-09 18:36:25 -0400

    add playRound function to play a single round of RPS and return a string with the results

--- javascript.js
+++ javascript.js
@@ -13,3 +13,49 @@ function getComputerChoice(){
             break;
     }
 }
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection==="rock"){
+        switch(computerSelection){
+            case "rock":
+                return("Tie!");
+                break;
+            case "paper":
+                return("You Lose! Paper beats Rock");
+                break;
+            case "scissors":
+                return("You Win! Rock beats Scissors");
+                break;
+        }
+    }
+    else if(playerSelection==="paper"){
+        switch(computerSelection){
+            case "rock":
+                return("You Win! Paper beats Rock");
+                break;
+            case "paper":
+                return("Tie!");
+                break;
+            case "scissors":
+                return("You Lose! Scissors beats Paper");
+                break;
+        }
+    }
+    else if(playerSelection==="scissors"){
+        switch(computerSelection){
+            case "rock":
+                return("You Lose! Rock beats Scissors");
+                break;
+            case "paper":
+                return("You Win! Scissors beats Paper");
+                break;
+            case "scissors":
+                return("Tie!");
+                break;
+        }
+    }
+}
