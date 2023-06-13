30eb81c988d4ff142212086939a4cac5ef14ef62
TristanNguyen0 <xkaura64@gmail.com>
2023-06-13 18:16:48 -0400

    Add UI elements so the user can press a selection of rock paper or scissors. First to 5 wins

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+//global variables for score keeping and displaying result
+let playerScore = 0;
+let computerScore = 0;
+const score = document.querySelector('div.score');
+score.innerText = `${playerScore} - ${computerScore}`;
+const result = document.querySelector('div.result');
+
+
 function getComputerChoice(){
     let rnd = Math.floor(Math.random() * (3-1+1))+1;
 
@@ -15,75 +23,90 @@ function getComputerChoice(){
 }
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection){
     playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
+    let computerSelection = getComputerChoice().toLowerCase();
+    
     if (playerSelection==="rock"){
         switch(computerSelection){
-            case "rock":
-                return("Tie! Rock vs. Rock");
+            case "rock":                
+                result.innerText = ("Tie! Rock vs. Rock");
                 break;
             case "paper":
-                return("You Lose! Paper beats Rock");
+                result.innerText = ("You Lose! Paper beats Rock");
+                computerScore++;
                 break;
             case "scissors":
-                return("You Win! Rock beats Scissors");
+                result.innerText = ("You Win! Rock beats Scissors");
+                playerScore++;
                 break;
         }
     }
     else if(playerSelection==="paper"){
         switch(computerSelection){
             case "rock":
-                return("You Win! Paper beats Rock");
+                result.innerText = ("You Win! Paper beats Rock");
+                playerScore++;
                 break;
             case "paper":
-                return("Tie! Paper vs. Paper");
+                result.innerText = ("Tie! Paper vs. Paper");
                 break;
             case "scissors":
-                return("You Lose! Scissors beats Paper");
+                result.innerText = ("You Lose! Scissors beats Paper");
+                computerScore++;
                 break;
         }
     }
     else if(playerSelection==="scissors"){
         switch(computerSelection){
             case "rock":
-                return("You Lose! Rock beats Scissors");
+                result.innerText = ("You Lose! Rock beats Scissors");
+                computerScore++;
                 break;
             case "paper":
-                return("You Win! Scissors beats Paper");
+                result.innerText = ("You Win! Scissors beats Paper");
+                playerScore++;
                 break;
             case "scissors":
-                return("Tie! Scissors vs. Scissors");
+                result.innerText = ("Tie! Scissors vs. Scissors");
                 break;
         }
+    }
+    score.innerText = `${playerScore} - ${computerScore}`; //update score
+    if (playerScore===5){ //once a player reaches 5 points
+        result.innerText = `Player wins!`;
+        playerScore = computerScore = 0; //reset game
+    }
+    else if (computerScore===5){
+        result.innerText = `Computer wins!`;
+        playerScore = computerScore = 0; //reset game
     }
 }
 
-function game(){
-    let computerScore = 0;
-    let playerScore = 0;
-    let result;
-    for (let i=0;i<5;i++){
-        let playerSelection = prompt("Enter rock, paper, or scissors");
-        let computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if(result.includes("Win")){
-            playerScore++;
-        }
-        else if(result.includes("Lose")){
-            computerScore++;
-        }
-    }
+// function game(){
+//     let computerScore = 0;
+//     let playerScore = 0;
+//     let result;
+//     for (let i=0;i<5;i++){
+//         let playerSelection = prompt("Enter rock, paper, or scissors");
+//         let computerSelection = getComputerChoice();
+//         result = playRound(playerSelection, computerSelection);
+//         console.log(result);
+//         if(result.includes("Win")){
+//             playerScore++;
+//         }
+//         else if(result.includes("Lose")){
+//             computerScore++;
+//         }
+//     }
     
-    if (playerScore>computerScore){
-        console.log(`Player Wins! ${playerScore} - ${computerScore}`);
-    }
-    else if(computerScore>playerScore){
-        console.log(`Computer Wins! ${playerScore} - ${computerScore}`);
-    }
-    else{
-        console.log(`Tie! ${playerScore} - ${computerScore}`);
-    }
-}
+//     if (playerScore>computerScore){
+//         console.log(`Player Wins! ${playerScore} - ${computerScore}`);
+//     }
+//     else if(computerScore>playerScore){
+//         console.log(`Computer Wins! ${playerScore} - ${computerScore}`);
+//     }
+//     else{
+//         console.log(`Tie! ${playerScore} - ${computerScore}`);
+//     }
+// }
